333245943bca1338791d9a57c1be705c3b0c4f03
Steve <ims12266@gmail.com>
2021-04-27 22:38:51 -0400

    Create Customer.js

---
+++ src/components/Customers.js
@@ -0,0 +1,15 @@
+import React from 'react';
+class Customers extends React.Component{
+    render(){
+        return(
+        <div>
+            <h2>홍길동</h2>
+            <p>961222</p>
+            <p>남자</p>
+            <p>대학생</p>
+        </div>
+        );
+
+    }
+}
+export default Customers;
